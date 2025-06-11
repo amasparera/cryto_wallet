461886502b2c9322a4a8da33bc1ea9b9671161b3
amasparera <amazparera@gmail.com>
2025-06-11 16:48:34 +0700

    start project

---
+++ lib/app/core/const/app_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/rendering.dart';
+
+class AppColor {
+  // primary colors
+  static const primaryBlue = Color(0XFF6552FE); // Purple
+  static const primaryGrey = Color(0XFF6C757D); // Grey
+  static const primaryWhite = Color(0XFFFFFFFF); // White
+  // secondary colors
+  static const secondaryGrey = Color(0XFF48D49E);
+  static const secondaryOrange = Color(0XFFFF8266); // Orange
+
+  
+}
---
+++ lib/app/core/const/app_icon.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppIcon {
+  static const navDashbord = Icons.dashboard_rounded;
+  static const navExchange = Icons.currency_exchange_rounded;
+  static const navUser = Icons.person_4_rounded;
+}
